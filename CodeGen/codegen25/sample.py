1d82eb83010a0ce04d30e491710985365388d84c
Mateo071 <floresmateo2000@gmail.com>
2024-01-15 19:00:18 -0500

    changed client folder name

--- CodeGen/codegen25/sample.py
+++ CodeGen/codegen25/sample.py
@@ -1,7 +1,8 @@
+import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-tokenizer = AutoTokenizer.from_pretrained("checkpoints/codegen25-7b-multi", trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained("checkpoints/codegen25-7b-multi")
-inputs = tokenizer("def hello_world():", return_tensors="pt")
+tokenizer = AutoTokenizer.from_pretrained("Salesforce/codegen25-7b-mono", trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained("Salesforce/codegen25-7b-mono")
+inputs = tokenizer("# this function prints hello world", return_tensors="pt")
 sample = model.generate(**inputs, max_length=128)
-print(tokenizer.decode(sample[0], truncate_before_pattern=[r"\n\n^#", "^'''", "\n\n\n"]))
+print(tokenizer.decode(sample[0]))
